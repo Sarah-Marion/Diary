15dca477c43f02764d587b3e878cd416c62811a8
Sarah-Marion <devsarahmarion@gmail.com>
2018-04-19 11:12:31 +0300

    added a highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,7 +6,8 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private elem: ElementRef) {
-    this.elem.nativeElement.style.backgroundColor = 'red'; 
+    this.elem.nativeElement.style.color = '#7700FD';
+    this.elem.nativeElement.style.fontSize = '20px'; 
   }
 
 //   @HostListener('mouseenter') onMouseEnter(){
